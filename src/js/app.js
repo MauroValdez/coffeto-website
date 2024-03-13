63c606a72f94a0ead86a25352ad393c167cd9e4e
Mauro Valdez <josenova0@gmail.com>
2024-03-13 16:24:00 -0300

    main-about

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,13 @@ import "@fontsource/pacifico";
 import "@fontsource/poppins/400.css";
 import "@fontsource/poppins/500.css";
 import "@fontsource/poppins/600.css";
+
+const header = document.querySelector('#header')
+console.log(header)
+
+const scrollHeader = () => {
+  window.scrollY >= 50 ? header.classList.add('scroll-header')
+                      : header.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
